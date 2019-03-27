ad9269014b6a6d8a5219f46016801d08d8569308
Swaroop Joshi <5982730+swaroopjcse@users.noreply.github.com>
2019-03-27 17:04:37 -0400

    README changed to include options based execution

--- README.txt
+++ README.txt
@@ -8,14 +8,20 @@ for the given test cases.
 
 Usage:
 
-$ java -jar <jar file> <test file>
+$ java -jar Core.jar <option> <test file>
+
+where
+	<option> includes
+		-t	Run tokenizer only
+		-p	Run parser only
+		-i	Run the full interpreter
 
 Example:
 
-$ java -jar Parser.jar data/test-pa2/Tests/validAllSimpleExpressions.txt 
+$ java -jar Core.jar -i data/test-pa3/validAllOneLine.core
 
 =================
 
 Author: Swaroop Joshi
 Email: joshi.127@osu.edu
-Date: 2018-10-25
+Date: 2019-03-27
--- run.fish
+++ run.fish
@@ -8,10 +8,17 @@
 #   echo '========================'
 # end
 
-for f in data/test-pa2/Tests/valid*core
-  set base (basename $f '.core')
-  echo '=== Testing ' $base '==='
-  java -jar Core.jar -p $f > /tmp/res
-  diff /tmp/res 'data/test-pa2/Results/'$base'.txt'
-  echo '========================'
+# for f in data/test-pa2/Tests/valid*core
+#   set base (basename $f '.core')
+#   echo '=== Testing ' $base '==='
+#   java -jar Core.jar -p $f > /tmp/res
+#   diff /tmp/res 'data/test-pa2/Results/'$base'.txt'
+#   echo '========================'
+# end
+
+for f in data/test-pa3/*.txt
+    set base (basename $f '.txt')
+    echo '=== Testing ' $base '==='
+    java -jar Core.jar -i $f
+    echo '========================'
 end
